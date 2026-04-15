68d8044d0dcb4c579a3e8d207664eeb1f0d407ac
Yeshwanth1897 <kesireddy.yeshwanth@gmail.com>
2026-04-15 12:37:19 +0530

    commit

---
+++ U2 S2 Q5.cpp
@@ -0,0 +1,23 @@
+/*Q5 – Destructor
+
+When a product object goes out of scope, the system should display the message:
+
+Product object destroyed
+
+Write a C++ program that demonstrates the use of a destructor in the class Product.*/
+
+#include <iostream>
+using namespace std;
+class Product {
+public:
+    Product() {
+        cout << "Product created\n";
+    }
+    ~Product() {
+        cout << "Product object destroyed\n";
+    }
+};
+int main() {
+    Product p1;
+    return 0;
+}
